fd21918a0412b390c5fd5c8e26b35cef5febcb63
sansern <sansern.par@gmail.com>
2020-09-18 06:23:28 +0700

    create controller mas_faculty

--- app/Mas_faculty.php
+++ app/Mas_faculty.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mas_faculty extends Model
 {
-    //
+    protected $table='mas_faculty';
+    protected $fillble=['faculty_id',
+        'faculty_code',
+        'ref_faculty_id',
+        'faculty_name'];
 }
